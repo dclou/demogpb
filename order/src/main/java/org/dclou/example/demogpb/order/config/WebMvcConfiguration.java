be17b617b95a91e12c9d36505e5773a5f29ce80f
Александр Подоплелов <alepod@gmail.com>
2017-06-14 22:05:45 +0300

    Пернос Swagger конфигурации в WebMvcConfiguration файл

--- order/src/main/java/org/dclou/example/demogpb/order/config/WebMvcConfiguration.java
+++ order/src/main/java/org/dclou/example/demogpb/order/config/WebMvcConfiguration.java
@@ -1,9 +1,19 @@
 package org.dclou.example.demogpb.order.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.paths.RelativePathProvider;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.servlet.ServletContext;
 
 /**
  * Created by Александр Подоплелов on 06.06.2017.
@@ -29,4 +39,22 @@ public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
         registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
     }
 
+}
+
+@Configuration
+@EnableSwagger2
+class SwaggerConfig {
+
+    @Autowired
+    ServletContext servletContext;
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .pathProvider(new RelativePathProvider(servletContext)) ;
+    }
 }
