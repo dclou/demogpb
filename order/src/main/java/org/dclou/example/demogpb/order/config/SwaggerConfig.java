b10689faec591a02ed3fd55b6b636e9aa98b2449
Александр Подоплелов <alepod@gmail.com>
2017-06-16 00:09:02 +0300

    Фильтр Order-интерфейса для API Swagger

--- order/src/main/java/org/dclou/example/demogpb/order/config/SwaggerConfig.java
+++ order/src/main/java/org/dclou/example/demogpb/order/config/SwaggerConfig.java
@@ -4,8 +4,10 @@ import com.google.common.base.Predicates;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.paths.RelativePathProvider;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -27,11 +29,21 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                //.apis(RequestHandlerSelectors.any())
                 .apis(RequestHandlerSelectors.basePackage("org.dclou.example.demogpb.order"))
-                //.apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
                 .paths(PathSelectors.any())
                 .build()
+                .apiInfo(apiInfo())
                 .pathProvider(new RelativePathProvider(servletContext)) ;
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Order Service API Tool")
+                .description("The metadata for API of the Order Application")
+                .version("1.0.0")
+                .termsOfServiceUrl("http://demo.zatona.com")
+                .license("LICENSE")
+                .licenseUrl("http://url-to-license.com")
+                .build();
     }
 }
