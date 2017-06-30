4336151db88e6e58fc0ecf9f62668ac3c0894ebe
Alexander Tumin <iamtakingiteasy@eientei.org>
2017-06-30 17:04:46 +0300

    Release ports

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,6 +4,6 @@ HOST="$1"
 
 mvn -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -s .travis.settings.xml -B package -DdeployDocker -DskipTests
 
-ssh user@$HOST 'ssh user@10.10.50.11 rm -rf demogpb'
-ssh user@$HOST 'ssh user@10.10.50.11 git clone https://github.com/dclou/demogpb'
-ssh user@$HOST 'ssh user@10.10.50.11 "cd demogpb && ./run.sh"'
+ssh git clone -oStrictHostKeyChecking=no -p50022 user@$HOST 'ssh user@10.10.50.11 rm -rf demogpb'
+ssh git clone -oStrictHostKeyChecking=no -p50022 user@$HOST 'ssh user@10.10.50.11 git clone https://github.com/dclou/demogpb'
+ssh git clone -oStrictHostKeyChecking=no -p50022 user@$HOST 'ssh user@10.10.50.11 "cd demogpb && ./run.sh"'
